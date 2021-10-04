e24b875f50a35f6ac94bd4ec03d99c9187adf3bc
whipowill <whipowill@protonmail.com>
2021-10-04 00:47:38 -0500

    Update.

---
+++ examples/from.sh
@@ -0,0 +1,3 @@
+cd "/c/games/diablo ii/save/"
+rm -rf Meir.*
+node /c/code/node-d2s-converter/convert.js "/c/users/username/saved games/diablo ii resurrected/Meir.d2s" "/c/games/diablo ii/save/Meir.d2s"
---
+++ examples/to.sh
@@ -0,0 +1,1 @@
+node /c/code/node-d2s-converter/convert.js "/c/games/diablo ii/save/Meir.d2s" "/c/users/username/saved games/diablo ii resurrected/Meir.d2s"
