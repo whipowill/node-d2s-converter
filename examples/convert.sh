8dfa244970dc3f3e8544a5abab6f665694a44fdf
whipowill <whipowill@protonmail.com>
2021-10-04 20:37:32 -0500

    Update.

--- examples/convert.sh
+++ examples/convert.sh
@@ -23,13 +23,15 @@ if [[ $1 = "lod" ]]
 then
 	find=".d2s"
 	replace=""
+	cd "$dir_d2lod"
+	rm -rf *.d2x
+	rm -rf *.ma*
+	rm -rf *.key
 	cd "$dir_d2res"
 	echo "SENDING SAVE FILES TO D2 LOD"
 	for file in *.d2s
 	do
 		echo "Converting file $file..."
-		short=${file/$find/$replace}
-		rm -rf "$short.*"
 		node "$converter" "$dir_d2res/$file" "$dir_d2lod/$file"
 	done
 fi
